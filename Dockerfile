ebe1a8e0b6b08dbfde1043cbeb5ebfeafa542bb0
Mike Kraus <mike.kraus@vantage-ai.com>
2021-12-10 15:30:56 +0100

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,30 @@
+FROM mcr.microsoft.com/azure-functions/python:3.0-python3.8-slim
+
+ENV AzureWebJobsScriptRoot=/home/site/wwwroot \
+    AzureFunctionsJobHost__Logging__Console__IsEnabled=true
+
+COPY . /home/site/wwwroot
+
+# build variables.
+ENV DEBIAN_FRONTEND noninteractive
+
+# install Microsoft SQL Server requirements.
+ENV ACCEPT_EULA=Y
+RUN apt-get update -y && apt-get update \
+  && apt-get install -y --no-install-recommends curl gcc g++ gnupg unixodbc-dev
+
+# Add SQL Server ODBC Driver 17 for Ubuntu 18.04
+RUN curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add - \
+  && curl https://packages.microsoft.com/config/debian/10/prod.list > /etc/apt/sources.list.d/mssql-release.list \
+  && apt-get update \
+  && apt-get install -y --no-install-recommends --allow-unauthenticated msodbcsql17 mssql-tools \
+  && echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile \
+  && echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
+
+# clean the install.
+RUN apt-get -y clean
+
+# install requirements.txt
+
+COPY requirements.txt /
+RUN pip install -r /requirements.txt
---
+++ TransactionsFunction/__init__.py
@@ -0,0 +1,110 @@
+import os
+import pandas as pd
+import azure.functions as func
+from azure.storage.blob import ContainerClient, BlobClient
+import pyodbc
+
+SERVER = os.environ.get("DB_SERVER")
+DB_NAME = os.environ.get("DB_NAME")
+USERNAME = os.environ.get("DB_USERNAME")
+PASSWORD = os.environ.get("DB_PASSWORD")
+CONN_STRING = os.environ.get("CONN_STRING")
+CONTAINER_NAME = 'data'
+TABLE_NAME = 'Transactions'
+
+
+def get_data(conn_str: str, container_name: str) -> pd.DataFrame:
+    """Fetches transactions csv files from Blob store, read into Pandas DataFrames and concatenate them all into one DataFrame.
+
+    Arguments:
+       conn_str (str): connection string of storage account in which the blob store resides
+       container_name (str): name of the container in which the csv files reside
+       
+    Returns:
+       data (pd.DataFrame): one single dataframe that contains all the fetched transactions
+    """
+
+    container = ContainerClient.from_connection_string(
+        conn_str=conn_str,
+        container_name = container_name
+        )
+
+    blob_list = container.list_blobs()
+
+    if not os.path.exists('./data'):
+        os.makedirs('./data')
+
+    dfs = []
+
+    for blob in blob_list:
+            
+        blob_client = BlobClient.from_connection_string(conn_str=conn_str,
+                                                        container_name=container_name, 
+                                                        blob_name=blob.name)
+        
+        with open(blob.name, "wb") as my_blob:
+                blob_data = blob_client.download_blob()
+                blob_data.readinto(my_blob)
+
+        raw_data = pd.read_csv(blob.name)
+
+        dfs.append(raw_data)
+        
+    data = pd.concat(dfs)
+
+    return data
+
+
+def write_to_sql(cursor: pyodbc.Cursor, data: pd.DataFrame, table_name: str) -> None:
+    """Inserts or updates rows of the input DataFrame.
+
+    Arguments:
+       cursor (pyodbc.Cursor): pyodbc database cursor
+       data (pd.DataFrame): DataFrame that contains the rows that ought to be written to the SQL database
+    """
+    
+    for index, row in data.iterrows():
+        cursor.execute(f'''MERGE INTO {table_name} as Target
+        USING (SELECT * FROM (VALUES (?, ?, ?, ?)) AS s (transaction_id, store, product, price)) AS Source
+        ON Target.transaction_id=Source.transaction_id
+        WHEN NOT MATCHED THEN
+        INSERT (transaction_id, store, product, price) VALUES (Source.transaction_id, Source.store, Source.product, Source.price)
+        WHEN MATCHED THEN
+        UPDATE SET transaction_id=Source.transaction_id, store=Source.store, product=Source.product, price=Source.price;
+        ''',
+                    row['transaction_id'],
+                    row['store'],
+                    row['product'],
+                    row['price'],
+                    )
+
+
+def main(mytimer: func.TimerRequest) -> None:
+    """The main function expected by the Azure Functions runtime
+    """
+
+    # fetch the data
+
+    data = get_data(CONN_STRING, CONTAINER_NAME)
+
+    # transform the data
+
+    data['transaction_id'] = data['transaction_id'].astype(int)
+    data['store'] = data['store'].astype(str)
+    data['product'] = data['product'].astype(str)
+    data['price'] = data['price'].astype(float)
+
+    data['product'] = data['product'].apply(lambda x: x.replace('shoo', 'shoe').lower())
+    
+    # connect with our SQL database
+
+    cnxn = pyodbc.connect(f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={SERVER};DATABASE={DB_NAME};UID={USERNAME};PWD={PASSWORD}', 
+                      autocommit=True,
+                      timeout=60)
+    
+    cursor = cnxn.cursor()
+    
+    # write the data to the SQL database
+
+    write_to_sql(cursor, data, TABLE_NAME)
+
